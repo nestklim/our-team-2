b140a39ce7fe701a44f6e43c013311da2e169983
nestklim <nestklim@mail.ru>
2016-12-21 18:16:43 +0200

    slider

---
+++ js/main.js
@@ -0,0 +1,15 @@
+;(function($) {
+	"use strict";
+
+
+	$('.ba-slider').slick({
+
+		prevArrow: '<img class="ba-angle ba-angle--left" src="../img/angle-left.png">',
+  		nextArrow: '<img class="ba-angle ba-angle--right" src="../img/angle-right.png">',
+  		dots: true
+
+	});
+
+
+})(jQuery);
+
