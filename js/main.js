d8d424112a17e564c9e9633d86ff8aa90f96f496
nestklim <nestklim@mail.ru>
2016-12-21 18:48:00 +0200

    img

--- js/main.js
+++ js/main.js
@@ -4,8 +4,8 @@
 
 	$('.ba-slider').slick({
 
-		prevArrow: '<img class="ba-angle ba-angle--left" src="../img/angle-left.png">',
-  		nextArrow: '<img class="ba-angle ba-angle--right" src="../img/angle-right.png">',
+		prevArrow: '<img class="ba-angle ba-angle--left" src="img/angle-left.png">',
+  		nextArrow: '<img class="ba-angle ba-angle--right" src="img/angle-right.png">',
   		dots: true
 
 	});
